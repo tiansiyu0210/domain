44340249b39ecd6a9f09e23fc1d3a0eb8efb863d
tiansiyu0210 <tiansiyu297@gmail.com>
2019-03-03 21:16:09 -0500

    Initial commit

---
+++ src/main/java/com/onevote/User.java
@@ -0,0 +1,47 @@
+package com.onevote;
+
+public class User {
+    private String id;
+    private String name;
+    private String role;
+
+    public User(){}
+
+    public User(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
