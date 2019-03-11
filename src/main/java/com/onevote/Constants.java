e0afc88f0c5e649ec78d8625c6d2914369ef2e41
tiansiyu0210 <tiansiyu297@gmail.com>
2019-03-10 22:45:56 -0400

    #2 add constants

---
+++ src/main/java/com/onevote/Constants.java
@@ -0,0 +1,8 @@
+package com.onevote;
+
+public class Constants {
+
+    /*kafka topic name*/
+    public static final String USER_TOPIC = "user";
+    public static final String VOTE_TOPIC = "vote";
+}
